93473f4ab73b46b6592d605bcbb1050affaf76fb
crorpi <crorpi@floridauniversitaria.es>
2017-02-15 10:56:42 +0100

    Update provision.sh
    update

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get -y install apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt
-sudo mysql_install_db
-sudo mysql_secure_installation
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get install mysql-client mysql-server -y
 sudo service apache2 restart
